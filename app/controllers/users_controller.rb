22c6c09b6fff5c30cab0a1a2adf08ef494daf398
MilanP5G <milan_parmar@hotmail.co.uk>
2020-06-03 10:49:03 +0100

    created basic user index page and refactored user#create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,10 @@ class UsersController < ApplicationController
   def home
   end
 
+  def index
+    @users = User.all
+  end
+
   def new
     @user = User.new
   end
@@ -10,10 +14,10 @@ class UsersController < ApplicationController
   def create
     @user = User.create(user_params)
     if @user.save
-      # session[:user_id] = @user.id
-      redirect_to user_path(@user)
+      session[:user_id] = @user.id
+      redirect_to users_path
     end
-    redirect_to new_user_path
+    "redirect_to(new_user_path) and return"
   end
 
   private
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
   has_many :owned_books, foreign_key: :owner_user_id, class_name: "Book"
   has_many :reflections, foreign_key: :reflection_user_id
   has_many :reflection_books, through: :reflections
+  has_secure_password
 
 
 end
