2d0a864c3b7fbe4808cd9ad49fd9380ff4cefcf5
MilanP5G <milan_parmar@hotmail.co.uk>
2020-06-03 12:28:47 +0100

    edited form_for books as named attr incorrectly and created index in BooksController

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,16 +1,23 @@
 class BooksController < ApplicationController
 
+  def index
+  end
+
   def new
     @book = Book.new
   end
 
   def create
-    @book = Book.create(book_params)
+    @book = Book.new(book_params)
     if @book.save
       redirect_to book_path(@book)
     else
       redirect_to new_book_path
     end
+  end
+
+  def show
+    @book = Book.find(params[:id])
   end
 
   private
