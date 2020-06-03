0968666054b5a786a5ece21a81cfa7fcbd75d2c5
MilanP5G <milan_parmar@hotmail.co.uk>
2020-06-03 16:48:25 +0100

    refactored links in views to direct to correct route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,7 +29,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:id, :username, :password, :email)
+    params.require(:user).permit(:username, :password, :email)
   end
 
 
