987857bd29ab090beaecec230dbbcbd42b7ac451
MilanP5G <milan_parmar@hotmail.co.uk>
2020-06-03 11:40:47 +0100

    added new and create to BooksController

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,18 @@
 class BooksController < ApplicationController
 
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.create(book_params)
+    if @book.save
+      redirect_to book_path(@book)
+    else
+      redirect_to new_book_path
+    end
+  end 
+
+
 
 end
