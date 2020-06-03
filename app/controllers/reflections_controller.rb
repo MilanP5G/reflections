e367654fa5fb98fe0a8e28bcba8b4123e28a07c0
MilanP5G <milan_parmar@hotmail.co.uk>
2020-06-03 11:49:26 +0100

    created new and create in ReflectionsController

--- app/controllers/reflections_controller.rb
+++ app/controllers/reflections_controller.rb
@@ -1,4 +1,19 @@
 class ReflectionsController < ApplicationController
 
+  def new
+    @reflection = Reflection.new
+  end
+
+  def create
+    @reflection = Reflection.create(reflection_params)
+    if @reflection.save
+      redirect_to reflection_path(@reflection.book)
+    else
+      redirect_to new_reflection_path
+    end
+  end 
+
+
+
 
 end
