e23ae929b333847d0451b7fefb7ca0d0c36a5dd0
MilanP5G <milan_parmar@hotmail.co.uk>
2020-06-05 18:12:25 +0100

    refactored create in sessions controller to authenticate password

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,9 +6,12 @@ class SessionsController < ApplicationController
   end
 
   def create
-    if @user = User.find_by(username: params[:username])
-      session[:user_id] = @user.id
-      redirect_to users_path
+    if User.find_by(username: params[:username])
+      @user = User.find_by(username: params[:username])
+        if @user && @user.authenticate(params[:password])
+          session[:user_id] = @user.id
+          redirect_to users_path
+       end
     else
       render :new
     end
